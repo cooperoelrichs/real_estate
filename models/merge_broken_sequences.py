845f7e0f786c3ade30f55a982cb04f4a522fb417
cooperoelrichs <c.oelrichs@gmail.com>
2017-02-08 12:41:45 +1100

    Add more checks and custom error types to improve testing.

--- models/merge_broken_sequences.py
+++ models/merge_broken_sequences.py
@@ -2,6 +2,17 @@ from datetime import timedelta
 import numpy as np
 import pandas as pd
 
+class DataValidationError(ValueError):
+    pass
+
+class ParallelListingsError(DataValidationError):
+    pass
+
+class UnbrokenListingsError(DataValidationError):
+    pass
+
+class DateEncountedError(DataValidationError):
+    pass
 
 class Merger(object):
     # for both sales and rentals
@@ -24,30 +35,56 @@ class Merger(object):
                 df['last_encounted'].isin(unbroken_last_encountereds[:-1]) &
                 ~ df['sequence_broken']
             ]
-            raise RuntimeError(
+            raise UnbrokenListingsError(
                 'There are unbroken records for multiple dates.\n' +
                 'Broken rows:\n%s' % str(damaged_rows)
             )
-        else:
-            pass
 
     def check_for_parrallel_listings(df):
-        unique_properties = df.drop_duplicates(
-            Merger.property_columns(df), keep='first'
+        property_columns = Merger.property_columns(df)
+        df = Merger.sort_df(df)
+
+        property_equality_with_next = (
+            df[property_columns] == df[property_columns].shift(-1)
+        ).all(axis=1)
+        parallel_with_next = (
+            df['last_encounted'] >= df['first_encounted'].shift(-1)
+        )
+        parallel_with_next[-1:] = False
+        parallel_listings_check = (
+            property_equality_with_next & parallel_with_next
         )
 
-        for row in unique_properties:
-            # get matches
-            # look for intersects in the dates
-            pass
+        if (parallel_listings_check).any():
+            parallel_listings_filter = (
+                parallel_listings_check | parallel_listings_check.shift(1)
+            )
+            parallel_listings_check[0] = parallel_listings_check[0]
+            parallel_listings = df[parallel_listings_filter]
 
-        print(df[Merger.property_columns(df)])
-        print(unique_properties)
+            raise ParallelListingsError(
+                'Parallel listings were found.\n' +
+                'Parallel listings:\n%s' % str(parallel_listings)
+            )
 
-        pass
+    def check_ordering_of_encounted_dates(df):
+        fe_gt_le = df['first_encounted'] > df['last_encounted']
+        if fe_gt_le.any():
+            raise DateEncountedError(
+                'Encounted dates failed the ordering check.\n' +
+                'Rows that failed the check:\n%s' % str(df[fe_gt_le])
+            )
 
     def property_columns(df):
         return df.columns.difference(Merger.NON_PROPERTY_COLUMNS)
+
+    def sort_df(df):
+        property_columns = Merger.property_columns(df)
+        df = df.sort_values(
+            by=list(property_columns) + ['first_encounted', 'last_encounted'],
+            inplace=False
+        )
+        return df
 
     def merge_on_price_change(df):
         # 1. Get duplicates
@@ -58,13 +95,13 @@ class Merger(object):
         # 5. Update the first_encountered column
         # 5. Return the filtered df
 
+        Merger.check_ordering_of_encounted_dates(df)
+        Merger.check_for_unbrokens(df)
+        Merger.check_for_parrallel_listings(df)
+
+        df = Merger.sort_df(df)
+
         property_columns = Merger.property_columns(df)
-
-        df = df.sort_values(
-            by=list(property_columns) + ['first_encounted', 'last_encounted'],
-            inplace=False
-        )
-
         time_diff = df['first_encounted'].shift(-1) - df['last_encounted']
         last_only = df.duplicated(subset=property_columns, keep='last')
         first_only = ~ df.duplicated(subset=property_columns, keep='first')
--- test/test_models/test_merge_broken_sequences.py
+++ test/test_models/test_merge_broken_sequences.py
@@ -2,7 +2,9 @@ import unittest
 from datetime import datetime
 import pandas as pd
 from pandas.util.testing import assert_frame_equal
-from real_estate.models.merge_broken_sequences import Merger
+from real_estate.models.merge_broken_sequences import (
+    Merger, ParallelListingsError, UnbrokenListingsError, DateEncountedError
+)
 
 
 class TestMerger(unittest.TestCase):
@@ -47,10 +49,17 @@ class TestMerger(unittest.TestCase):
         columns=TEST_COLUMNS
     )
 
+    DF_WITH_FE_GT_LE = pd.DataFrame(
+        data=[
+            [datetime(2017, 2, 1), datetime(2017, 1, 1)],
+        ],
+        columns=['first_encounted', 'last_encounted']
+    )
+
     DF_WITH_PARRALLELS = pd.DataFrame(
         data=[
             ['a', 1, 'x', 1, 2, datetime(2017, 1, 1), datetime(2017, 3, 1), True],
-            ['a', 1, 'x', 3, 3, datetime(2017, 1, 1), datetime(2017, 3, 1), False],
+            ['a', 1, 'x', 3, 3, datetime(2017, 2, 1), datetime(2017, 3, 1), False],
             ['b', 1, 'x', 4, 4, datetime(2017, 1, 1), datetime(2017, 2, 1), True],
             ['b', 1, 'x', 4, 4, datetime(2017, 2, 2), datetime(2017, 3, 1), False],
         ],
@@ -58,20 +67,25 @@ class TestMerger(unittest.TestCase):
     )
 
     def test_merger(self):
-        # assert_frame_equal(
-        #     self.EXPECTED_DF,
-        #     Merger.merge_on_price_change(self.TEST_DF)
-        # )
-        pass
+        assert_frame_equal(
+            self.EXPECTED_DF,
+            Merger.merge_on_price_change(self.TEST_DF)
+        )
 
     def test_check_for_unbrokens(self):
         self.assertRaises(
-            RuntimeError,
+            UnbrokenListingsError,
             Merger.check_for_unbrokens, self.DF_WITH_UNBROKENS
+        )
+
+    def test_check_ordering_of_encounted_dates(self):
+        self.assertRaises(
+            DateEncountedError,
+            Merger.check_ordering_of_encounted_dates, self.DF_WITH_FE_GT_LE
         )
 
     def test_check_for_parrallel_listings(self):
         self.assertRaises(
-            RuntimeError,
+            ParallelListingsError,
             Merger.check_for_parrallel_listings, self.DF_WITH_PARRALLELS
         )
