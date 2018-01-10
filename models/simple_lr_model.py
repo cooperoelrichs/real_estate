5b3fc08617023d81de5558a10fc0919eb168b58c
cooperoelrichs <c.oelrichs@gmail.com>
2018-01-10 18:09:16 +1100

    Keep both the ridge and lr models.

--- models/simple_lr_model.py
+++ models/simple_lr_model.py
@@ -7,12 +7,26 @@ class LinearModel(PriceModel):
     HAS_FEATURE_IMPORTANCE = False
 
     def __init__(self, X, y, X_labels):
+        self.model = LinearRegression(
+            fit_intercept=True,
+            normalize=False,
+            copy_X=True,
+            n_jobs=-1
+        )
+
+        self.setup_self(X, y, X_labels)
+
+class RidgeModel(PriceModel):
+    HAS_SIMPLE_COEFS = True
+    HAS_FEATURE_IMPORTANCE = False
+    MODEL_APLHA = 0.1
+
+    def __init__(self, X, y, X_labels):
         self.model = Ridge(
             alpha=self.MODEL_APLHA,
             fit_intercept=True,
-            normalize=True,
+            normalize=False,
             copy_X=True,
-            # n_jobs=1
         )
 
         self.setup_self(X, y, X_labels)
