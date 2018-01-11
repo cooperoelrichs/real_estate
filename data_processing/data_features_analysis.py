7b688db24b501112ece72d5049807571e25a8ecd
cooperoelrichs <c.oelrichs@gmail.com>
2018-01-11 17:12:40 +1100

    Clip categorical features to reasonable ranges.

--- data_processing/data_features_analysis.py
+++ data_processing/data_features_analysis.py
@@ -16,9 +16,9 @@ class DataFeaturesAnalysis():
 
         def tables(df, feature_analysis_dir):
             pivots = [
-                ('property_type', 'bedrooms'),
-                ('suburb', 'bedrooms'),
-                ('suburb', 'property_type'),
+                ('property_type', 'bedrooms', 8),
+                ('suburb', 'bedrooms', 8),
+                ('suburb', 'property_type', None),
             ]
             DataFeaturesAnalysis.table_pivots(
                 pivots, df, feature_analysis_dir
@@ -29,8 +29,12 @@ class DataFeaturesAnalysis():
             )
 
         def table_pivots(pivot_pairs, df, outputs_dir):
-            for a, b in pivot_pairs:
-                count_matrix = DataFeaturesAnalysis.pivot_table(a, b, df)
+            for a, b, cap in pivot_pairs:
+                if cap is not None:
+                    count_matrix = DataFeaturesAnalysis.pivot_table_with_cap(
+                        a, b, cap, df)
+                else:
+                    count_matrix = DataFeaturesAnalysis.pivot_table(a, b, df)
                 name = DataFeaturesAnalysis.join_names((a, b))
                 file_path = outputs_dir + name + '.html'
                 DataAnalysis.save_df_as_html(count_matrix, file_path)
@@ -40,6 +44,16 @@ class DataFeaturesAnalysis():
                 df.loc[:, (a, b)],
                 index=a, columns=b, aggfunc=len, fill_value=0
             )
+
+        def pivot_table_with_cap(a, b, cap, df):
+            x = df.loc[:, (a, b)].copy()
+            x.loc[x[b] > cap, b] = cap
+            table = DataFeaturesAnalysis.pivot_table(a, b, x)
+
+            cols = [a for a in table.columns]
+            cols[-1] = ('%i+' % cols[-1])
+            table.columns = cols
+            return table
 
         def table_counts(columns, df, outputs_dir):
             for column in columns:
@@ -52,42 +66,50 @@ class DataFeaturesAnalysis():
 
         def plots(df, feature_analysis_dir):
             bar_feature_groups = [
-                (('bedrooms',), None),
-                (('bathrooms',), None),
-                (('garage_spaces',), None),
-                (('property_type',), None)
+                (('bedrooms',), None, (None,), 12),
+                (('bathrooms',), None, (None,), 12),
+                (('garage_spaces',), None, (None,), 12),
+                (('property_type',), None, (None,), 12)
             ]
             DataFeaturesAnalysis.plot_feature_groups(
                 bar_feature_groups, df, feature_analysis_dir, 'bar'
             )
 
             barh_feature_groups = [
-                (('suburb',), (4, 16)),
-                (('property_type', 'bedrooms'), None),
-                (('suburb', 'bedrooms'), (6, 16 * 4))
+                (('suburb',), (4, 30), (None,), 6),
+                (('property_type', 'bedrooms'), (6, 10), (None, 8), 8),
+                # (('suburb', 'bedrooms'), (4, 16*4), (None, 8), 6)
             ]
             DataFeaturesAnalysis.plot_feature_groups(
                 barh_feature_groups, df, feature_analysis_dir, 'barh'
             )
 
         def plot_feature_groups(groups, df, outputs_dir, plot_type):
-            for features, figure_size in groups:
+            for features, figure_size, caps, font_size in groups:
                 group_name = DataFeaturesAnalysis.join_names(features)
                 file_name = 'property_count_by_%s.png' % group_name
                 DataFeaturesAnalysis.plot_property_count_by(
                     features, df,
                     outputs_dir + file_name,
                     plot_type,
-                    figure_size
+                    figure_size,
+                    caps,
+                    font_size
                 )
 
         def join_names(strs):
             return reduce(lambda a, b: a + '_' + b, strs)
 
-        def plot_property_count_by(features, df, output_file, plot_type, figure_size):
+        def plot_property_count_by(
+            features, df, output_file, plot_type, figure_size, caps, font_size
+        ):
+            x = df.loc[:, features].copy()
+            for i, a in enumerate(features):
+                if caps[i] is not None:
+                    x.loc[x[a] > caps[i], a] = caps[i]
+
             plt.figure()
-            df.groupby(features).size().sort_index().plot(
-                kind=plot_type, figsize=figure_size
-            )
+            counts = x.groupby(features).size().sort_index()
+            counts.plot(kind=plot_type, figsize=figure_size, fontsize=font_size)
             plt.tight_layout()
             plt.savefig(output_file)
