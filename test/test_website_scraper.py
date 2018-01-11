eec3dc44d52fa22579883c69e29c8c3fa2784bf4
cooperoelrichs <c.oelrichs@gmail.com>
2018-01-12 10:51:16 +1100

    .

--- test/test_website_scraper.py
+++ test/test_website_scraper.py
@@ -2,6 +2,8 @@ import unittest
 from real_estate.website_scraper import WebsiteScraper
 import real_estate.real_estate_property as rep
 
+from scraper.test.test_page_scraper import populate_state_and_postcode
+
 
 class TestWebsiteScraper(unittest.TestCase):
     def test_group_scrapings(self):
@@ -18,6 +20,7 @@ class TestWebsiteScraper(unittest.TestCase):
             ),
             rep.DataContentTypeNotSupported('')
         ]
+        tests = populate_state_and_postcode(tests, 'test_state', 9999)
 
         parsed, failed = WebsiteScraper.split_scrapings(tests)
         self.assertEqual(parsed, tests[:1])
