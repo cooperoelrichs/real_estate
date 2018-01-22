504110eade9bf555a85fe0ae4e8f486002177334
cooperoelrichs <c.oelrichs@gmail.com>
2018-01-22 16:03:46 +1100

    Support pasing params in.

--- models/simple_lr_model.py
+++ models/simple_lr_model.py
@@ -6,15 +6,24 @@ class LinearModel(PriceModel):
     HAS_SIMPLE_COEFS = True
     HAS_FEATURE_IMPORTANCE = False
 
-    def __init__(self, X, y, X_labels):
-        self.model = LinearRegression(
-            fit_intercept=True,
-            normalize=False,
-            copy_X=True,
-            n_jobs=-1
-        )
+    PARAMS = {
+        'fit_intercept': True,
+        'normalize': False,
+        'copy_X': True,
+        'n_jobs': -1
+    }
+
+    def __init__(self, X, y, X_labels, params=None):
+        if params == None:
+            self.model = LinearModel.make_model(self.PARAMS)
+        else:
+            self.model = LinearModel.make_model(params)
 
         self.setup_self(X, y, X_labels)
+
+    def make_model(params):
+        return LinearRegression(**params)
+
 
 class RidgeModel(PriceModel):
     HAS_SIMPLE_COEFS = True
