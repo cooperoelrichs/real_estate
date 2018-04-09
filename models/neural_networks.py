4efd2bfbbde6e72146b2a77adf741d22df0219bc
cooperoelrichs <c.oelrichs@gmail.com>
2018-04-09 16:59:16 +1000

    .

--- models/neural_networks.py
+++
@@ -1,8 +0,0 @@
-from sklearn.neural_networks import MLPRegressor
-from real_estate.models.price_model import PriceModel
-
-
-class Perceptron(PriceModel):
-    MODEL_CLASS = MLPRegressor
-    def __init__(self, ):
-        pass
