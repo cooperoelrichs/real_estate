88496989d94a962cfab4d5fd85a605583e50362b
cooperoelrichs <c.oelrichs@gmail.com>
2018-05-03 17:49:53 +1000

    Don't display the pyplot plots, only save images.

--- models/live_keras_plotter.py
+++ models/live_keras_plotter.py
@@ -6,11 +6,12 @@ from keras.callbacks import Callback
 
 class LivePlotter(Callback):
     A = 'val_'
-    def __init__(self, figsize, num_epochs, outputs_dir):
+    def __init__(self, figsize, num_epochs, outputs_dir, name):
         super().__init__()
         self.figsize = figsize
         self.num_epochs = num_epochs
         self.outputs_dir = outputs_dir
+        self.name = name
         self.metric_names = []
         self.data = {}
         self.epochs = []
@@ -35,16 +36,27 @@ class LivePlotter(Callback):
             self.data[name]['t'].append(results[name])
             self.data[name]['v'].append(results[self.A + name])
 
+            if epoch < 2:
+                starting_epoch = 0
+            else:
+                # Exclude the first epoch, removing very large values.
+                starting_epoch = 1
+
+            x = self.epochs[starting_epoch:]
+            y_t = self.data[name]['t'][starting_epoch:]
+            y_v = self.data[name]['v'][starting_epoch:]
+
             self.axes[i].clear()
             self.axes[i].set_title(name)
             self.axes[i].set_xlabel('epoch')
-            self.axes[i].plot(self.epochs, self.data[name]['t'], label='training')
-            self.axes[i].plot(self.epochs, self.data[name]['v'], label='validation')
+            self.axes[i].plot(x, y_t, label='training')
+            self.axes[i].plot(x, y_v, label='validation')
             self.axes[i].legend()
 
-        # plt.tight_layout()
-        plt.draw()
-        plt.pause(0.01)
+        plt.tight_layout()
+        # plt.draw()
+        # plt.pause(0.01)
 
-        if (epoch + 1) == self.num_epochs:
-            plt.savefig(os.path.join(self.outputs_dir, 'results-by-epoch.png'))
+        plt.savefig(os.path.join(
+            self.outputs_dir, 'results-by-epoch-%s.png' % self.name
+        ))
